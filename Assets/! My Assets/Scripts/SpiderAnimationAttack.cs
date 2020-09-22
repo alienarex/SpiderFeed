b42e5482faec6bfe29a931b92aad7ad96ded73f2
alienarex <annica@alienus.se>
2020-09-22 20:35:55 +0200

    Moved logic for attacking and eat humans. Soundeffect added here aswell for the sound when human gets eaten

--- Assets/! My Assets/Scripts/SpiderAnimationAttack.cs
+++ Assets/! My Assets/Scripts/SpiderAnimationAttack.cs
@@ -1,28 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Assets.__My_Assets.Scripts.CountdownControl;
 
 public class SpiderAnimationAttack : StateMachineBehaviour
 {
+    private AudioSource _audioSource;
+
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        var test = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
-
+        animator.gameObject.GetComponent<BoxCollider>().enabled = true;
+        animator.gameObject.GetComponent<BoxCollider>().isTrigger = true;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         // Here's where I put code that will be exevuted during the animatiobn
-        var test = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+        int hitRange = 20;
+        RaycastHit hit;
+        Vector3 forward = animator.transform.TransformDirection(Vector3.forward);
+        Vector3 origin = animator.transform.position;
 
+        if (Physics.Raycast(origin, forward, out hit, hitRange))
+        {
+            if (hit.transform.gameObject.tag == "Human")
+            {
+                PlaySoundWithInterval(1.5f, 0.5f);
+
+                IncreaseCountdown(hit.transform.gameObject.GetComponent<Human>().TimeValue);
+                hit.transform.gameObject.SendMessage("SetInactiveHuman");
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Find and plays the soundeffect for eating humans
+    /// </summary>
+    /// <param name="audioDuration"></param>
+    /// <param name="audioStartPoint"></param>
+    void PlaySoundWithInterval(float audioDuration, float audioStartPoint = 0.01f)
+    {
+        // ref: https://forum.unity.com/threads/soundchannel-cpp.371808/#post-2411098
+        _audioSource = GameObject.Find("CrunchEffect").GetComponent<AudioSource>();
+        _audioSource.time = Mathf.Min(audioDuration, _audioSource.clip.length - audioStartPoint);
+
+        _audioSource.Play();
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        var test = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+        var testar = animator.gameObject.GetComponent<BoxCollider>().enabled = false;
+        animator.gameObject.GetComponent<BoxCollider>().isTrigger = false;
 
     }
 

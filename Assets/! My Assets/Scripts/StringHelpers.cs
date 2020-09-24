023f85d115c93ed1038949d708e243bb94e6543c
alienarex <annica@alienus.se>
2020-09-24 13:50:47 +0200

    New String helper class

---
+++ Assets/! My Assets/Scripts/StringHelpers.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.__My_Assets.Scripts
+{
+    public static class StringHelpers
+    {
+
+        /// <summary>
+        /// Converts seconds to minutes and seconds
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static string ConvertTimeToString(this float time)
+        {
+            float minutes = Mathf.FloorToInt(time / 60);
+            float seconds = Mathf.FloorToInt(time % 60);
+            return string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+}

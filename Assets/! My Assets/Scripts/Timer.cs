aebc0b705d26818a00c47e0cc1ff2bd35d2d305d
alienarex <annica@alienus.se>
2020-09-24 13:49:34 +0200

    Modyfied to a singleton

--- Assets/! My Assets/Scripts/Timer.cs
+++ Assets/! My Assets/Scripts/Timer.cs
@@ -1,56 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
-public class Timer : MonoBehaviour
+namespace Assets.__My_Assets.Scripts
 {
-    // REf: https://gamedevbeginner.com/how-to-make-countdown-timer-in-unity-minutes-seconds/
-    float timeRemaining;
-    public bool timerIsRunning = false;
-    public Text timeText;
-
-    private void Start()
+    public sealed class Timer
     {
-        //timeRemaining = PlayerPrefs.GetFloat("initialGamingTime");
-        timeRemaining = 180;//testning
-
-        // Starts the timer automatically
-        timerIsRunning = true;
-    }
-
-    void Update()
-    {
-        if (timerIsRunning)
+        // REf: https://gamedevbeginner.com/how-to-make-countdown-timer-in-unity-minutes-seconds/
+        private Timer()
         {
-            if (timeRemaining > 0)
-            {
-                timeRemaining -= Time.deltaTime;
-            }
-            else
-            {
-                Debug.Log("Time has run out!");
-                timeRemaining = 0;
-                timerIsRunning = false;
-            }
-            DisplayTime(timeRemaining);
         }
-    }
+        private static Timer instance = null;
+        public static Timer Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Timer();
+                }
+                return instance;
+            }
+        }
 
-    /// <summary>
-    /// Extracts minutes and seconds from deltatime
-    /// </summary>
-    /// <param name="timeToDisplay"></param>
-    void DisplayTime(float timeToDisplay)
-    {
-        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
-        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        public float TimeRemaining { get; private set; }
+        public float TotalTime { get; private set; }
 
-        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-    }
+        bool timerIsRunning;
 
-    public void IncreaseTimer(float secondsToAdd)
-    {
-        timeRemaining += secondsToAdd;
+        /// <summary>
+        /// Updates timer values
+        /// </summary>
+        public void UpdateTimer()
+        {
+            if (timerIsRunning)
+            {
+                if (TimeRemaining > 0)
+                {
+                    TimeRemaining -= Time.deltaTime;
+                    TotalTime += Time.deltaTime;
+                }
+                else
+                {
+                    TimeRemaining = 0;
+                    timerIsRunning = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Increase seconds to timer
+        /// </summary>
+        /// <param name="seconds"></param>
+        public void AddSecondsToTimer(float seconds)
+        {
+            TimeRemaining += seconds;
+            timerIsRunning = true;
+        }
     }
 }
